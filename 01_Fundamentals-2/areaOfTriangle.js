66bb9f17b179314017b6f061edfc180502adf735
Aritra Nandi <aritranandi30@gmail.com>
2025-07-09 22:09:17 +0530

    calculator and area of triangle

---
+++ 01_Fundamentals-2/areaOfTriangle.js
@@ -0,0 +1,3 @@
+let len = 20, h = 30;
+let area = 0.5 * len * h;
+console.log("Area of triangle", area);
---
+++ 01_Fundamentals-2/calculator.js
@@ -0,0 +1,8 @@
+let num1 = 35;
+let num2 = 10;
+
+console.log("addition", num1 + num2);
+console.log("subtraction", num1 - num2);
+console.log("multiplication", num1 * num2);
+console.log("division", num1 / num2);
+console.log("remainder", num1 % num2);
