81d5a52033f96eb94772619b0ced313ce9b84b57
Aritra Nandi <aritranandi30@gmail.com>
2025-07-10 22:42:33 +0530

    discount calculator

---
+++ 01_Fundamentals-2/discountCalculator.js
@@ -0,0 +1,6 @@
+let originalValue = 50.75,
+  discountPercent = 15.5;
+
+let discountedPrice = originalValue - (originalValue * discountPercent) / 100;
+
+console.log("Discounted Price = ", discountedPrice);
