bd254371a4099efd4c7932cdbf2f0dd32404e8a2
Aritra Nandi <aritranandi30@gmail.com>
2025-07-10 22:37:12 +0530

    average and type conversion

---
+++ 01_Fundamentals-2/average.js
@@ -0,0 +1,9 @@
+let student1Score = "65",
+  student2Score = "92",
+  student3Score = "86";
+let averageScore =
+  (parseInt(student1Score) +
+    parseInt(student2Score) +
+    parseInt(student3Score)) /
+  3;
+console.log("average", averageScore);
