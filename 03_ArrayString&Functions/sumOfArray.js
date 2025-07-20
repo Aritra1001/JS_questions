f386a8f08dfe687583a3b3df40e002da2447111b
Aritra Nandi <aritranandi30@gmail.com>
2025-07-20 23:40:14 +0530

    sum of all even numbers in the array

---
+++ 03_ArrayString&Functions/sumOfArray.js
@@ -0,0 +1,24 @@
+/**
+ *  Your goal is to find the sum of all even numbers in the array, but you need to implement a special condition:
+ *  if you encounter a number that is a multiple of 5, you should skip it and move on to the next element without
+ *  including it in the sum.
+
+
+
+Expected Input
+[1,6,8,3,5,20,10]
+Expected Output
+14
+ */
+
+function sumOfArray(arr){
+    let sum = 0;
+    for(let i of arr){
+        if(i % 2 !== 0 || i % 5 === 0){
+            continue;
+        }
+        sum += i;
+    }
+    return sum;
+}
+console.log(sumOfArray([1,6,8,3,5,20,10]));
